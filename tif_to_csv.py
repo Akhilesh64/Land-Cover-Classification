8335cd9fd725a3c601e13d71dfd6043401d26016
Akhilesh Sharma <akhilesharma47@gmail.com>
2020-10-12 11:49:16 +0530

    Add files via upload

---
+++ tif_to_csv.py
@@ -0,0 +1,12 @@
+#This script requires raster2xyz wheel to run
+#pip install raster2xyz
+
+from raster2xyz.raster2xyz import Raster2xyz
+import os
+from glob import glob
+
+for infile in glob(os.getcwd() + '\*.tif'):
+    file, ext1 = os.path.splitext(infile)
+    rtxyz = Raster2xyz()
+    rtxyz.translate(infile, file+'.csv')
+
